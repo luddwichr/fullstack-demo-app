e0292d53626428f90f9e67731250681e8d4bfa33
Marco Spiess <marco.spiess@andrena.de>
2022-02-04 18:28:21 +0100

    fix test isolation

--- backend/src/test/java/com/example/demo/dailynews/DailyNewsIntegrationTest.java
+++ backend/src/test/java/com/example/demo/dailynews/DailyNewsIntegrationTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -32,6 +33,7 @@ public class DailyNewsIntegrationTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @DirtiesContext
     @Test
     void getAll() throws Exception {
         final var expectedTitle = "some title";
@@ -50,6 +52,7 @@ public class DailyNewsIntegrationTest {
         assertThat(firstNews.message()).isEqualTo(expectedMessage);
     }
 
+    @DirtiesContext
     @Test
     void postAndGetSingle() throws Exception {
         final var expectedTitle = "someTitle";
